4c273308c113f1ee4cc3d49b500912c70b453c8c
Carlos Prieto <cmprieto@gmail.com>
2021-02-01 17:21:08 +0100

    Nivel 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
+import { Container, Row, Col, Button, Jumbotron } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  const [joke, setJoke] = useState([]);
-  const [counter, setCounter] = useState(0);
+    const [joke, setJoke] = useState([]);
+    const [counter, setCounter] = useState(0);
 
-    const clickHandler=()=>{    //Funcion para cambiar valor estado y entonces cuenta otro chiste        
-        setCounter(counter+1)
+    const clickHandler = () => {    //Funcion para cambiar valor estado y entonces cuenta otro chiste        
+        setCounter(counter + 1)
     }
 
-  useEffect(() => {
-    axios
-      .get(`https://icanhazdadjoke.com/`, {
-        headers: {
-          Accept: "application/json",
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        setJoke(res.data.joke);
-      })
-  }, [counter]); //al variar valor estado 'contar' se actualiza la petición de nuevo chiste
+    useEffect(() => {
+        axios
+            .get(`https://icanhazdadjoke.com/`, {
+                headers: {
+                    Accept: "application/json",
+                },
+            })
+            .then((res) => {
+                console.log(res);
+                setJoke(res.data.joke);
+            })
+    }, [counter]); //al variar valor estado 'contar' se actualiza la petición de nuevo chiste
 
-  return (
-    <div>
-        <h3>Jokes for everybody!</h3>
-        <p>{joke}</p>
-        <button onClick={clickHandler}>Next Joke!</button>
-    </div>
-);
+    return (
+        <div>
+            <Container >
+                <Row className="justify-content-center">
+                    <Col xs={9} className="mt-5">
+                        <Jumbotron className="bg-white text-center">
+                            <h6 className="display-4">Jokes for everybody! &#128514;</h6>
+                            <p className="lead">{joke}</p>
+                            <Button onClick={clickHandler}>Next Joke! </Button>
+                        </Jumbotron>
+                    </Col>
+
+                </Row>
+
+            </Container>
+        </div>
+    );
 };
+
 export default App;
